56472a3de1ed14510d3c254e1dd975a666ed99b2
o0nekov0o <nydeanor@gmail.com>
2024-05-19 16:24:53 +0200

    Tests Saving : - CRM_User - Customer Remaining : - Contract - Event

--- CRM_EpicEvents/Website_CRM/models.py
+++ CRM_EpicEvents/Website_CRM/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator
 from location_field.models.plain import PlainLocationField
@@ -14,25 +16,21 @@ class CRM_User(AbstractUser):
 
 
 class Customer(models.Model):
-    creator = models.ForeignKey(to=CRM_User, on_delete=models.CASCADE,
-                                null=True, blank=False)
+    creator = models.ForeignKey(to=CRM_User, on_delete=models.CASCADE, null=True, blank=False)
     information = models.TextField(max_length=2048, null=True, blank=True)
     full_name = models.CharField(max_length=128, null=False, blank=False)
-    email = models.EmailField(max_length=128, null=False, blank=False)
+    email = models.EmailField(max_length=128, null=False, blank=False, unique=True)
     phone_number = PhoneNumberField(region="FR", null=False, blank=False)
     enterprise_name = models.CharField(max_length=128, null=False, blank=False, )
     creation_date = models.DateField("Date", auto_now_add=True)
     last_update = models.DateField("Date", auto_now=True)
     commercial_contact = models.ForeignKey(to=CRM_User, on_delete=models.CASCADE,
-                                           related_name='customer_abstract_commercial')
+                                           related_name='customer_commercial_contact')
 
 
 class Contract(models.Model):
-    customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE,
-                                 related_name='contract_customer_all')
-    commercial_contact = models.ForeignKey(to=Customer, on_delete=models.CASCADE,
-                                           related_name='contract_customer_contact',
-                                           default='customer.commercial_contact')
+    customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
+    commercial_contact = models.CharField(max_length=128, null=False, blank=True)
     total_amount = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0)])
     unpaid_amount = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0)])
     creation_date = models.DateField("Date", auto_now_add=True)
@@ -41,18 +39,36 @@ class Contract(models.Model):
     contract_state = models.IntegerField(choices=STATE_CHOICES, default=0, null=False, blank=False)
 
 
+@receiver(post_save, sender=Contract)
+def auto_create_commercial_contact(instance, **kwargs):
+    """
+    Anytime a Contract object is created, associate customer data thanks to customer
+    """
+    if kwargs.get('created', False):
+        if instance.customer:
+            instance.commercial_contact = instance.customer.commercial_contact.username
+            instance.save(update_fields=['commercial_contact'])
+
+
 class Event(models.Model):
     contract = models.ForeignKey(to=Contract, on_delete=models.CASCADE)
-    customer_name = models.ForeignKey(to=Customer, on_delete=models.CASCADE,
-                                      related_name='event_customer_name',
-                                      default='contract.customer.full_name')
-    customer_contact = models.ForeignKey(to=Customer, on_delete=models.CASCADE,
-                                         related_name='event_customer_contact',
-                                         default='contract.customer.commercial_contact')
+    customer_name = models.CharField(max_length=128, null=False, blank=True)
+    customer_contact = models.CharField(max_length=128, null=False, blank=True)
     event_start_date = models.DateTimeField(null=False, blank=False)
     event_end_date = models.DateTimeField(null=False, blank=False)
-    support_contact = models.ForeignKey(to=Customer, on_delete=models.CASCADE,
-                                        related_name='event_contact', null=True, blank=True)
+    support_contact = models.ForeignKey(to=CRM_User, on_delete=models.CASCADE, null=True, blank=True)
     location = PlainLocationField(based_fields=['city'], zoom=7, null=False, blank=False)
     attendees = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0)])
     notes = models.TextField(max_length=2048, null=True, blank=True)
+
+
+@receiver(post_save, sender=Event)
+def auto_create_customer_data(instance, **kwargs):
+    """
+    Anytime an Event object is created, associate customer data thanks to customer
+    """
+    if kwargs.get('created', False):
+        if instance.contract:
+            instance.customer_name = instance.contract.customer.full_name
+            instance.customer_contact = instance.contract.customer.commercial_contact.username
+            instance.save(update_fields=['customer_name', 'customer_contact'])
--- CRM_EpicEvents/Website_CRM/tests.py
+++ CRM_EpicEvents/Website_CRM/tests.py
@@ -1,3 +1,120 @@
+import phonenumbers
 from django.test import TestCase
+from rest_framework import status
+from Website_CRM import models, serializers
 
-# Create your tests here.
+
+class TestCaseCrmUser(TestCase):
+
+    def setUp(self):
+        self.new_user_1_data = {
+            'username': 'o0nekov0o',
+            'password': 'test_123',
+            'first_name': 'admin',
+            'last_name': 'webmaster',
+            'email': 'admin@gmail.com',
+            'collaborator_type': 0,
+        }
+        self.new_user_1_test = models.CRM_User.objects.create_user(**self.new_user_1_data)
+
+    def test_crm_user_model(self):
+        self.new_user_2_data = {
+            'creator': self.new_user_1_test,
+            'username': 'new_user',
+            'password': 'new_password',
+            'first_name': 'donald',
+            'last_name': 'mitchell',
+            'email': 'mitch@gmail.com',
+            'collaborator_type': 0,
+        }
+        self.new_user_2_test = models.CRM_User.objects.create_user(**self.new_user_2_data)
+        self.assertEqual(self.new_user_1_test, self.new_user_2_test.creator)
+        self.assertNotEqual(self.new_user_1_test, self.new_user_2_test)
+
+    def test_crm_user_serializer(self):
+        self.new_user_1_data['username'] = 'o0nekov0o_1'
+        self.crm_user_serializer = serializers.CrmUserSerializer(data=self.new_user_1_data)
+        self.assertTrue(self.crm_user_serializer.is_valid())
+
+    def test_crm_user_view(self):
+        self.client.login(username=self.new_user_1_data['username'],
+                          password=self.new_user_1_data['password'])
+        self.new_user_1_data['username'] = 'o0nekov0o_2'
+        self.new_user_1_data['email'] = 'admin@gmail.com'
+        response = self.client.post('/crm_users/', self.new_user_1_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.new_user_1_data['username'] = 'o0nekov0o'
+        self.new_user_1_data['email'] = 'admin_2@gmail.com'
+        response = self.client.post('/crm_users/', self.new_user_1_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.new_user_1_data['username'] = 'o0nekov0o_2'
+        self.new_user_1_data['email'] = 'admin_2@gmail.com'
+        response = self.client.post('/crm_users/', self.new_user_1_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class TestCaseCustomer(TestCase):
+
+    def setUp(self):
+        self.new_user_1_data = {
+            'username': 'o0nekov0o',
+            'password': 'test_123',
+            'first_name': 'admin',
+            'last_name': 'webmaster',
+            'email': 'admin@gmail.com',
+            'collaborator_type': 1,
+        }
+        self.new_user_1_test = models.CRM_User.objects.create_user(**self.new_user_1_data)
+        self.new_customer_1_data = {
+            'information': 'New Customer Unlocked',
+            'full_name': 'John Doe the One',
+            'email': 'johndoe_theone@gmail.com',
+            'phone_number': '+33701020304',
+            'enterprise_name': 'Anonymous Enterprise',
+            'commercial_contact': self.new_user_1_test
+        }
+        self.new_customer_1_test = models.Customer.objects.create(**self.new_customer_1_data)
+
+    def test_customer_model(self):
+        self.new_customer_2_data = {
+            'information': 'Another Customer Unlocked',
+            'full_name': 'John Doe the Second',
+            'email': 'johndoe_numbertwo@gmail.com',
+            'phone_number': '+33702030405',
+            'enterprise_name': 'Anonymous Enterprise',
+            'commercial_contact': self.new_user_1_test
+        }
+        self.new_customer_2_test = models.Customer.objects.create(**self.new_customer_2_data)
+        self.assertEqual(self.new_customer_1_test.commercial_contact,
+                         self.new_customer_2_test.commercial_contact)
+        self.assertNotEqual(self.new_customer_1_test, self.new_customer_2_test)
+
+    def test_customer_serializer(self):
+        self.new_customer_1_data['email'] = 'johndoe_number3@gmail.com'
+        self.new_customer_1_data['phone_number'] = '+33733457295'
+        self.new_customer_1_data['commercial_contact'] = \
+            models.CRM_User.objects.get(username='o0nekov0o').id
+        self.customer_serializer = serializers.CustomerSerializer(data=self.new_customer_1_data)
+        self.assertTrue(self.customer_serializer.is_valid())
+
+    def test_customer_view(self):
+        self.client.login(username=self.new_user_1_data['username'],
+                          password=self.new_user_1_data['password'])
+        self.new_customer_1_data['email'] = 'johndoe_number4@gmail.com'
+        self.new_customer_1_data['phone_number'] = '+33701020304'
+        self.new_customer_1_data['commercial_contact'] = \
+            models.CRM_User.objects.get(username='o0nekov0o').id
+        response = self.client.post('/customers/', self.new_customer_1_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.new_customer_1_data['email'] = 'johndoe_theone@gmail.com'
+        self.new_customer_1_data['phone_number'] = '+33733457296'
+        self.new_customer_1_data['commercial_contact'] = \
+            models.CRM_User.objects.get(username='o0nekov0o').id
+        response = self.client.post('/customers/', self.new_customer_1_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.new_customer_1_data['email'] = 'johndoe_number4@gmail.com'
+        self.new_customer_1_data['phone_number'] = '+33733457296'
+        self.new_customer_1_data['commercial_contact'] = \
+            models.CRM_User.objects.get(username='o0nekov0o').id
+        response = self.client.post('/customers/', self.new_customer_1_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
