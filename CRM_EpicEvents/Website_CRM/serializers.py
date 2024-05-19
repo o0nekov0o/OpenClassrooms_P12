5504cf26690ddfc76974a589b9c3a2e39365f291
o0nekov0o <nydeanor@gmail.com>
2024-05-19 16:32:01 +0200

    Updating of models, serializers and views

--- CRM_EpicEvents/Website_CRM/serializers.py
+++ CRM_EpicEvents/Website_CRM/serializers.py
@@ -11,28 +11,45 @@ class PermissionSerializer(serializers.HyperlinkedModelSerializer):
         fields = []
 
 
-class CustomerSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = models.Customer
-        exclude = ['information', 'full_name', 'email', 'phone_number',
-                   'enterprise_name', 'commercial_contact', 'url']
-
-
 class CrmUserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.CRM_User
         fields = ['username', 'password', 'first_name', 'last_name', 'email', 'collaborator_type', 'url']
 
 
+class FilterForCustomer(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        return models.CRM_User.objects.filter(collaborator_type=1)
+
+
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    commercial_contact = FilterForCustomer(many=False)
+
+    class Meta:
+        model = models.Customer
+        fields = ['information', 'full_name', 'email', 'phone_number',
+                  'enterprise_name', 'commercial_contact', 'url']
+
+
 class ContractSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Contract
         fields = ['customer', 'commercial_contact', 'total_amount',
-                  'unpaid_amount', 'creation_date', 'contract_state', 'url']
+                  'unpaid_amount', 'contract_state', 'url']
+        extra_kwargs = {'commercial_contact': {'read_only': True}, }
+
+
+class FilterForEvent(serializers.PrimaryKeyRelatedField):
+    def get_queryset(self):
+        return models.CRM_User.objects.filter(collaborator_type=2)
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
+    support_contact = FilterForEvent(many=False)
+
     class Meta:
         model = models.Event
         fields = ['contract', 'customer_name', 'customer_contact', 'event_start_date',
                   'event_end_date', 'support_contact', 'location', 'attendees', 'notes', 'url']
+        extra_kwargs = {'customer_name': {'read_only': True}, 'customer_contact': {'read_only': True}, }
+
--- CRM_EpicEvents/Website_CRM/views.py
+++ CRM_EpicEvents/Website_CRM/views.py
@@ -88,6 +88,12 @@ class CustomerViewSet(viewsets.ModelViewSet):
         if information == '' or full_name == '' or email == '' or phone_number == '' \
                 or enterprise_name == '' or commercial_contact == '':
             return response.Response(status=status.HTTP_401_UNAUTHORIZED)
+        if not email == instance.email:
+            if models.Customer.objects.filter(Q(email=email)):
+                return response.Response(status=status.HTTP_401_UNAUTHORIZED)
+        if not phone_number == instance.phone_number:
+            if models.Customer.objects.filter(Q(phone_number=phone_number)):
+                return response.Response(status=status.HTTP_401_UNAUTHORIZED)
         if request.user.is_superuser or request.user.collaborator_type == 0 \
                 or instance.creator == request.user:
             password = serializer.validated_data['password']
@@ -107,6 +113,8 @@ class CustomerViewSet(viewsets.ModelViewSet):
             new_customer['enterprise_name'], new_customer['commercial_contact']
         if information == '' or full_name == '' or email == '' or phone_number == '' \
                 or enterprise_name == '' or commercial_contact == '':
+            return response.Response(status=status.HTTP_401_UNAUTHORIZED)
+        if models.Customer.objects.filter(Q(email=email) | Q(phone_number=phone_number)):
             return response.Response(status=status.HTTP_401_UNAUTHORIZED)
         if request.user.is_superuser or request.user.collaborator_type in [0, 1]:
             models.Customer.objects.create(information=information, full_name=full_name, email=email,
@@ -210,12 +218,10 @@ class ContractViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         changed_contract = serializer.validated_data
-        customer, commercial_contact, total_amount, unpaid_amount, creation_date, contract_state = \
-            changed_contract['customer'], changed_contract['commercial_contact'], \
-            changed_contract['total_amount'], changed_contract['unpaid_amount'], \
-            changed_contract['creation_date'], changed_contract['contract_state']
-        if customer == '' or commercial_contact == '' or total_amount == '' or unpaid_amount == '' \
-                or creation_date == '' or contract_state == '':
+        customer, total_amount, unpaid_amount, contract_state = \
+            changed_contract['customer'], changed_contract['total_amount'], \
+            changed_contract['unpaid_amount'],  changed_contract['contract_state']
+        if customer == '' or total_amount == '' or unpaid_amount == '' or contract_state == '':
             return response.Response(status=status.HTTP_401_UNAUTHORIZED)
         if request.user.is_superuser or request.user.collaborator_type == 0 \
                 or instance.customer.creator == request.user:
@@ -228,16 +234,14 @@ class ContractViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=self.request.data)
         serializer.is_valid(raise_exception=True)
         new_contract = serializer.validated_data
-        customer, commercial_contact, total_amount, unpaid_amount, creation_date, contract_state = \
-            new_contract['customer'], new_contract['commercial_contact'], new_contract['total_amount'], \
-            new_contract['unpaid_amount'], new_contract['creation_date'], new_contract['contract_state']
-        if customer == '' or commercial_contact == '' or total_amount == '' or unpaid_amount == '' \
-                or creation_date == '' or contract_state == '':
+        customer, total_amount, unpaid_amount, contract_state = \
+            new_contract['customer'], new_contract['total_amount'], \
+            new_contract['unpaid_amount'], new_contract['contract_state']
+        if customer == '' or total_amount == '' or unpaid_amount == '' or contract_state == '':
             return response.Response(status=status.HTTP_401_UNAUTHORIZED)
         if request.user.is_superuser or request.user.collaborator_type == 0:
-            models.Contract.objects.create(customer=customer, commercial_contact=commercial_contact,
-                                           total_amount=total_amount, unpaid_amount=unpaid_amount,
-                                           creation_date=creation_date, contract_state=contract_state)
+            models.Contract.objects.create(customer=customer, total_amount=total_amount,
+                                           unpaid_amount=unpaid_amount, contract_state=contract_state)
             return response.Response(status=status.HTTP_201_CREATED)
         else:
             return response.Response(status=status.HTTP_401_UNAUTHORIZED)
@@ -266,15 +270,12 @@ class EventsViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         changed_event = serializer.validated_data
-        contract, customer_name, customer_contact, event_start_date, \
-        event_end_date, support_contact, location, attendees, notes = \
-            changed_event['contract'], changed_event['customer_name'], \
-            changed_event['customer_contact'], changed_event['event_start_date'], \
+        contract, event_start_date, event_end_date, support_contact, location, attendees, notes = \
+            changed_event['contract'], changed_event['event_start_date'], \
             changed_event['event_end_date'], changed_event['support_contact'], \
             changed_event['location'], changed_event['attendees'], changed_event['notes']
-        if contract == '' or customer_name == '' or customer_contact == '' \
-                or event_start_date == '' or event_end_date == '' or support_contact == '' \
-                or location == '' or attendees == '' or notes == '':
+        if contract == '' or event_start_date == '' or event_end_date == '' \
+                or support_contact == '' or location == '' or attendees == '' or notes == '':
             return response.Response(status=status.HTTP_401_UNAUTHORIZED)
         if request.user.is_superuser or request.user.collaborator_type == 0 \
                 or instance.contract.customer.creator == request.user \
@@ -288,21 +289,17 @@ class EventsViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=self.request.data)
         serializer.is_valid(raise_exception=True)
         new_event = serializer.validated_data
-        contract, customer_name, customer_contact, event_start_date, \
-        event_end_date, support_contact, location, attendees, notes = \
-            new_event['contract'], new_event['customer_name'], \
-            new_event['customer_contact'], new_event['event_start_date'], \
+        contract, event_start_date, event_end_date, support_contact, location, attendees, notes = \
+            new_event['contract'], new_event['event_start_date'], \
             new_event['event_end_date'], new_event['support_contact'], \
             new_event['location'], new_event['attendees'], new_event['notes']
-        if contract == '' or customer_name == '' or customer_contact == '' \
-                or event_start_date == '' or event_end_date == '' or support_contact == '' \
-                or location == '' or attendees == '' or notes == '':
+        if contract == '' or event_start_date == '' or event_end_date == '' \
+                or support_contact == '' or location == '' or attendees == '' or notes == '':
             return response.Response(status=status.HTTP_401_UNAUTHORIZED)
         if request.user.is_superuser or request.user.collaborator_type == 0 \
                 or request.user.collaborator_type == 1 and contract.contract_state == 1 \
                 and contract.customer.creator == request.user:
-            models.Event.objects.create(contract=contract, customer_name=customer_name,
-                                        customer_contact=customer_contact, event_start_date=event_start_date,
+            models.Event.objects.create(contract=contract, event_start_date=event_start_date,
                                         event_end_date=event_end_date, support_contact=support_contact,
                                         location=location, attendees=attendees, notes=notes)
             return response.Response(status=status.HTTP_201_CREATED)
